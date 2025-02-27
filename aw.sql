7c93ccc2da36fe44ed2a5fdcee13298dfcf42bec
Yundie77 <147044947+Yundie77@users.noreply.github.com>
2025-02-27 21:13:30 +0100

    update sql

--- aw.sql
+++ aw.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-02-2025 a las 19:45:04
+-- Tiempo de generación: 27-02-2025 a las 21:12:53
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `gastos` (
   `id` int(11) NOT NULL,
   `usuario_id` int(11) NOT NULL,
-  `categoria` enum('Ropa','Comida','Ocio','Salud','Transporte') NOT NULL,
+  `categoria` varchar(50) NOT NULL,
   `monto` decimal(10,2) NOT NULL,
   `fecha` date NOT NULL,
   `comentario` text DEFAULT NULL
@@ -41,9 +41,29 @@ CREATE TABLE `gastos` (
 --
 
 INSERT INTO `gastos` (`id`, `usuario_id`, `categoria`, `monto`, `fecha`, `comentario`) VALUES
-(6, 1, 'Ropa', 33.00, '2025-01-30', ''),
-(7, 1, 'Ropa', 34.00, '2025-02-09', ''),
-(8, 1, 'Ropa', 22.00, '2025-01-29', '');
+(3, 1, 'Ropa', 33.00, '2025-02-04', ''),
+(4, 1, 'Ropa', 2.00, '2025-02-12', ''),
+(5, 1, 'Ropa', 23.00, '2025-02-07', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `email`, `password`) VALUES
+(1, 'Ejemplo', 'ejemplo@correo.com', 'claveEncriptada');
 
 --
 -- Índices para tablas volcadas
@@ -57,6 +77,13 @@ ALTER TABLE `gastos`
   ADD KEY `usuario_id` (`usuario_id`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -64,7 +91,13 @@ ALTER TABLE `gastos`
 -- AUTO_INCREMENT de la tabla `gastos`
 --
 ALTER TABLE `gastos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
@@ -74,7 +107,7 @@ ALTER TABLE `gastos`
 -- Filtros para la tabla `gastos`
 --
 ALTER TABLE `gastos`
-  ADD CONSTRAINT `gastos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+  ADD CONSTRAINT `gastos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
