b9581185c98cf325d43bc91d75b9e63b871fd227
Vagram <Vagramsaakyan@gmail.com>
2025-03-06 22:03:49 +0100

    grupos implementados + cambios pequeños

--- includes/vistas/comun/nav.php
+++ includes/vistas/comun/nav.php
@@ -1,8 +1,9 @@
 <?php
 // Barra de navegación común
 ?>
+
 <nav style="text-align: center;">
   <a href="/aw/index.php">Inicio</a>
   <a href="/aw/gastos.php">Gastos</a>
+  <a href="/aw/grupos.php">Grupos</a>
 </nav>
-<hr>
