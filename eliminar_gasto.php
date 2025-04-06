78abea9dd5e3c9b1960cfb30fc4a815ad11be1d6
Yundie77 <147044947+Yundie77@users.noreply.github.com>
2025-04-06 18:32:37 +0200

    cambio pequeno en elimina gasto

--- eliminar_gasto.php
+++ eliminar_gasto.php
@@ -3,21 +3,21 @@ session_start();
 require_once 'includes/config.php';
 $app = \es\ucm\fdi\aw\Aplicacion::getInstance();
 $conn = $app->getConexionBd();
+
 $id = isset($_GET['id']) ? intval($_GET['id']) : 0;
 if ($id <= 0) {
     die("ID inválido");
 }
 
 $user_id = $_SESSION['user_id'];
-$sql = "DELETE FROM gastos WHERE id = ? AND usuario_id = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("ii", $id, $user_id);
-if ($stmt->execute()) {
+$gastos = new \es\ucm\fdi\aw\Gastos($conn);
+
+if ($gastos->eliminarGasto($id, $user_id)) {
     header("Location: historial_gastos.php");
     exit;
 } else {
     echo "Error al eliminar el registro.";
 }
-$stmt->close();
+
 $conn->close();
 ?>
